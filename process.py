523b4ba95a93d169abe94888d967175a2de1fbf0
vkatari10 <kaskat2021@gmail.com>
2025-08-18 20:10:30 -0400

    Added requirements.txt and CLI script shell

---
+++ process.py
@@ -0,0 +1,19 @@
+s#! /usr/bin/python3
+"""
+CLI Script to process input CSVs from CubeSpace EOS
+Simulation Results
+
+Author: Vikas Katari
+Date: 08/18/2025
+"""
+
+
+import sys
+
+# import module w/ actual code here
+
+if __name__ == "__main__":
+
+    args = sys.argv
+
+    file = args[1]
